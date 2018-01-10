1c4fed5bddbfc75a6181c52c976d4ed7a2676719
pfakriadis <pfakriadis@gmail.com>
2018-01-10 23:40:31 +0200

    Addition of attributes of Doctor class

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,4 +1,46 @@
 
 public class Doctor {
 
+	private int arMitrwou;
+	private String lastname;
+	private String firstname;
+	private int telephone;
+	private String speciality;
+	private String department;
+	public int getArMitrwou() {
+		return arMitrwou;
+	}
+	public void setArMitrwou(int arMitrwou) {
+		this.arMitrwou = arMitrwou;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	public String getSpeciality() {
+		return speciality;
+	}
+	public void setSpeciality(String speciality) {
+		this.speciality = speciality;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 }
