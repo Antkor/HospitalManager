3ca8abedac6ab64f0a52ba0e7010001b9d4a9e72
pfakriadis <pfakriadis@gmail.com>
2018-01-14 14:47:30 +0200

    IMplementation of getDoctors method in DoctoList class

--- src/DoctorList.java
+++ src/DoctorList.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
 
 public class DoctorList {
 
+	public static ArrayList<Doctor> getDoctors() {
+		DbConnector connector = new DbConnector();
+		return connector.getAllDoctors();
+	}
 }
